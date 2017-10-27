44e02952da28a6230fd3f28c6be0eb9caa598b91
cjhaas <chris@vendiadvertising.com>
2017-10-27 11:14:58 -0500

    Code cleanup

--- tests/test-parser_command.php
+++ tests/test-parser_command.php
@@ -1,9 +1,11 @@
-<?php
-
+<?php declare(strict_types=1);
 namespace Vendi\LogParser\Tests;
 
-use \PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\TestCase;
 
+/**
+ * @coversNothing
+ */
 class test_parser_command extends TestCase
 {
     public function test_nothing()
